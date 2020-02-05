1bdd6d3de21582fa3964e3d10a554411889b3c5f
jleyv <jxl9382@g.rit.edu>
2020-02-05 18:25:44 -0500

    First commit

---
+++ MainProgram.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Diagnostics;
+
+/**
+ * Author: Jorge Leyva
+ * Program that will generate large prime numbers using the C# parallel Libraries
+ */
+namespace PrimeGen
+{
+    class Program
+    {
+        /*
+         * The usageMessage method displays a help message on how to properly use the program whenever incorrect
+         * usage of the program is done
+         */
+        public static void UsageMessage()
+        {
+            Console.WriteLine("dotnet run PrimeGen <bits> <count=1>");
+            Console.WriteLine("    - bits - the number of bits of the prime number, this must be a multiple of 8, " +
+                              "and at least 32 bits");
+            Console.WriteLine("    - count - the number of prime numbers to generates, defaults to 1");
+            Environment.Exit(0);
+        }
+        
+        static void Main(string[] args)
+        {
+            int bits = 0;
+            int count = 1;
+            if (args.Length == 1)
+            {
+                try
+                {
+                    bits = int.Parse(args[0]);
+                }
+                catch (Exception e)
+                {
+                    UsageMessage();
+                    throw e;
+                }
+                if (bits < 32 || bits % 8 != 0)
+                {
+                    UsageMessage();
+                }
+            }
+            if (args.Length == 2)
+            {
+                try
+                {
+                    bits = int.Parse(args[0]);
+                    count = int.Parse(args[1]);
+                }
+                catch (Exception e)
+                {
+                    UsageMessage();
+                    throw e;
+                }
+                if (bits < 32 || bits % 8 != 0)
+                {
+                    UsageMessage();
+                }
+            }
+
+            if (args.Length > 2 || args.Length < 1)
+            {
+                UsageMessage();
+            }
+            
+            var png = new PrimeNumGen(bits, count);
+            Stopwatch stopwatch = new Stopwatch();
+            
+            Console.WriteLine("dotnet run {0} {1}", bits, count);
+            Console.WriteLine("BitLength: {0}", bits);
+
+            stopwatch.Start();
+            png.run();
+            stopwatch.Stop();
+
+            Console.WriteLine("Time to Generate: {0}", stopwatch.Elapsed);
+        }
+    }
+}
---
+++ PrimeNumGen.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace PrimeGen
+{
+    using Extensions;
+    
+    /*
+     * The PrimeNumGen class is used to create a number of prime numbers in parallel based on the bit number input and
+     * how many prime numbers are needed, with one prime number being the default.  This class utilizes the
+     * RNGCryptoServiceProvider to use a cryptographically secure sequence of bits to create the prime numbers.
+     */
+    public class PrimeNumGen
+    {
+        private static object myLock = new Object();
+        private RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+        private int bits;
+        private int count;
+        private int counter;
+
+        /*
+         * Constructor of PrimeNumGen object
+         */
+        public PrimeNumGen(int bits, int count)
+        {
+            this.bits = bits;
+            this.count = count;
+        }
+
+        /*
+         * the makeAPrimeNumber method uses the bit count given to it by the PrimeNumGen object to create a potentially
+         * prime number 
+         */
+        public BigInteger makeAPotentialPrimeNumnber(int numOfBits)
+        {
+            byte[] randNum = new byte[numOfBits/8];
+            rng.GetBytes(randNum);
+            BigInteger primeNum = new BigInteger(randNum);
+            return primeNum;
+        }
+        
+        /*
+         * this run method utilizes the C# parallel library to create a number of prime numbers in parallel.
+         */
+        public void run()
+        {
+            Parallel.For(0, Int32.MaxValue, (i, state) =>
+            {
+                if (counter == count)
+                {
+                    state.Break();
+                }
+
+                BigInteger potentialPrimeNum = makeAPotentialPrimeNumnber(bits);
+                
+                lock (myLock)
+                {
+                    if (potentialPrimeNum.IsProbablyPrime())
+                    {
+                        BigInteger zero = BigInteger.Zero;
+                            BigInteger two = new BigInteger(2);
+                            int comparison = potentialPrimeNum.CompareTo(zero);
+                            if (counter != count & comparison >= 0 & !BigInteger.Remainder(potentialPrimeNum, two).IsZero)
+                            {
+                                counter++;
+                                Console.WriteLine("{0}: {1}", counter, potentialPrimeNum);
+                                Console.WriteLine();   
+                            }
+                    } 
+                }
+            });
+        }
+    }
+    
+}
